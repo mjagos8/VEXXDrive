f414ec37c8a47ad91b05e333c2da78b97575b6dd
matt <84691857+mjagos8@users.noreply.github.com>
2022-11-07 20:20:24 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,8 @@
 // backL                motor         10              
 // frontR               motor         2               
 // backR                motor         1               
-// Limit1               limit         H               
-// Limit2               limit         G               
+// TopLimit             limit         H               
+// BottomLimit          limit         G               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -37,24 +37,27 @@ brain brain;
 // motor frontR = motor(PORT2, ratio18_1,false);
 // motor backR = motor(PORT1, ratio18_1,false);
 motor flywheel = motor(PORT13, ratio18_1,false);
-motor feeder = motor(PORT12, ratio18_1,false);
-bool feederdirection = false;
+motor feeder = motor(PORT15, ratio18_1,false);
+motor intake = motor(PORT14, ratio18_1,false);
+bool feederdirection = true;
 bool flywheelOn = false;
+bool intakeOn = false;
+bool endgameOn = false;
 
 void userController(void) 
 {
+  Brain.Screen.clearScreen(); 
   while (1) 
   {
-    
   
  int x = Controller1.Axis4.value();
  int y = Controller1.Axis3.value();
  
  
-  backL.spin(vex::directionType::fwd, Controller1.Axis3.value() - Controller1.Axis1.value() + Controller1.Axis4.value(), vex::velocityUnits::pct);
-  frontL.spin(vex::directionType::fwd, Controller1.Axis3.value() - Controller1.Axis1.value() - Controller1.Axis4.value(), vex::velocityUnits::pct);
-  backR.spin(vex::directionType::rev, Controller1.Axis3.value() + Controller1.Axis1.value() - Controller1.Axis4.value(), vex::velocityUnits::pct);
-  frontR.spin(vex::directionType::rev, Controller1.Axis3.value() + Controller1.Axis1.value() + Controller1.Axis4.value(), vex::velocityUnits::pct);
+  backL.spin(vex::directionType::fwd, Controller1.Axis3.value() + Controller1.Axis1.value() + Controller1.Axis4.value(), vex::velocityUnits::pct);
+  frontL.spin(vex::directionType::fwd, Controller1.Axis3.value() + Controller1.Axis1.value() - Controller1.Axis4.value(), vex::velocityUnits::pct);
+  backR.spin(vex::directionType::rev, Controller1.Axis3.value() - Controller1.Axis1.value() - Controller1.Axis4.value(), vex::velocityUnits::pct);
+  frontR.spin(vex::directionType::rev, Controller1.Axis3.value() - Controller1.Axis1.value() + Controller1.Axis4.value(), vex::velocityUnits::pct);
 
   //North East
   if (x > 25  && y > 25 ){
@@ -77,52 +80,73 @@ void userController(void)
   frontR.stop(brakeType::brake);
   }
 
-  else if( Controller1.ButtonR2.pressing()){ 
-    if(flywheelOn)
-    {
-      flywheelOn = false;
-    }
-    else{
-      flywheelOn = true;
-    }
-}
 
-else if (Controller1.ButtonB.pressing()){
-  if(Limit1.pressing())
+
+  
+
+  if (Controller1.ButtonB.pressing()) 
+  {
+    if(feederdirection)
+    {
+      feeder.spin(directionType::rev, 100, velocityUnits::pct);
+    }
+    else
+    {
+      feeder.spin(directionType::fwd, 100, velocityUnits::pct);
+    }
+  }
+
+if(TopLimit.value() == 1)
   {
     feederdirection = false;
   }
-  else if(Limit2.pressing())
+if(BottomLimit.value() == 1)
   {
     feederdirection = true;
   }
   
-  if(feederdirection)
-  {
-    feeder.spin(directionType::rev, 100, velocityUnits::pct);
-  }
-    else
-  {
-    feeder.spin(directionType::fwd, 100, velocityUnits::pct);
-  }
-  
-  }
 
+
+
+if( Controller1.ButtonY.pressing())
+{
+  if(intakeOn)
+{
+    intake.stop(brakeType::brake);
+    intakeOn = false;
+    wait(250, msec);
+
+}
 else{
-feeder.stop(brakeType::brake);
-
-if(flywheelOn)
-{
-  flywheel.spin(directionType::rev, 100, velocityUnits::pct);
-}  
-else
-{
-  flywheel.stop(brakeType::brake);
+    intake.spin(directionType::rev, 100, velocityUnits::pct);
+  intakeOn = true;
+  wait(250, msec);
 }
 }
 
+if( Controller1.ButtonR1.pressing())
+{
+  if(flywheelOn)
+{
+    flywheel.stop(brakeType::brake);
+    flywheelOn = false;
+    wait(250, msec);
+
 }
+else{
+    flywheel.spin(directionType::rev, 100, velocityUnits::pct);
+  flywheelOn = true;
+  wait(250, msec);
 }
+
+}
+
+}
+
+}
+
+
+
 
 int main() {
   Competition.drivercontrol(userController);
